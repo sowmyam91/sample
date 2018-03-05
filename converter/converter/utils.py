a4ebd15264723854cc203a1ad882c73128a2e410
Sowmya M <sowmyam91@gmail.com>
2018-03-05 17:37:17 +0530

    method to convert json to tosca

--- converter/converter/utils.py
+++ converter/converter/utils.py
@@ -1,5 +1,6 @@
 import xmltodict
 import json
+from pyhocon import HOCONConverter, ConfigFactory
 
 
 class ConverterUtils:
@@ -59,6 +60,52 @@ class ConverterUtils:
             return self.tag_list
 
 
+class ToscaConverter:
+    def __init__(self, data):
+        self.data = data
+
+    @staticmethod
+    def format_key(key, pattern=":"):
+        return key.split(pattern)[1]
+
+    @staticmethod
+    def render_to_template(**data):
+        config = ConfigFactory.from_dict(data)
+        return HOCONConverter.convert(config, 'hocon')
+
+    # @staticmethod
+    # def get_data_properties(**data):
+    #     if not isinstance(data, dict):
+    #         return dict(type=type(data).__name__,
+    #                              title=value,
+    #                              desc=value,
+    #                              optional=True
+    #                              )
+    #     else:
+    #         return
+    #
+    #     return properties
+
+    def convert_json_to_tosca(self):
+        print(self.data)
+        tempalte_args = []
+        for k, v in self.data.iteritems():
+            value = self.format_key(k)
+            template_dict = dict(type=type(v).__name__,
+                                 title=value,
+                                 desc=value,
+                                 optional=True
+                                 )
+            # if type(v).__name__ == "dict":
+            #     properties = self.get_data_properties(**value)
+            #     template_dict.update(properties=properties)
+            tempalte_args.append({value: template_dict})
+
+        tosca_data = {"tosca": tempalte_args}
+        temp = ToscaConverter.render_to_template(**tosca_data)
+        return temp
+
+
 if __name__ == "__main__":
     with open('../data/001-http-response-getInventoryResponse.xml', 'r') as fd:
         xml_data = fd.read()
@@ -72,3 +119,6 @@ if __name__ == "__main__":
 
     with open('../data/sample1.json', 'w') as f:
         f.write(json.dumps(data, indent=4))
+
+    me = ToscaConverter(data.get('me')[0]).convert_json_to_tosca()
+    print me
